cbc23f6ef298add40c9c3f64b95bef05723ee816
Light0912 <romain.terreno@gmail.com>
2021-02-11 12:09:36 +0100

    fix some bug

--- config/route.php
+++ config/route.php
@@ -1,11 +1,13 @@
+
 <?php
 
 use vendor\System\Router;
 $router = new Router();
 
 $router->setRoute("",  "HomeController/index");
-$router->setRoute("login",  "AuthenticationController/_Login");
-$router->setRoute("register",  "AuthenticationController/_Register", "register");
+$router->setRoute("login",  "LoginController/index");
+//$router->setRoute("login",  "AuthenticationController/_Login");
+//$router->setRoute("register",  "AuthenticationController/_Register", "register");
 $router->setRoute("data", "DataController/index", 'index', ['GET', 'POST']);
 $router->setRoute("capteur", "CapteurController/index");
 $router->setRoute("docs", "DocumentationController/home", 'HomeDocView');

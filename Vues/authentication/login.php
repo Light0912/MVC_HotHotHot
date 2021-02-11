d7c461d880b8ccea79d8ea04437f3d9641600574
DuvalQuentin <DuvalQuentin83@gmail.com>
2021-02-11 11:20:32 +0100

    fix

--- Vues/authentication/login.php
+++ Vues/authentication/login.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= $request->getSiteUrl() ?>css/authentication.css">
     <title>Identification d'utilisateur</title>
 </head>
--- config/route.php
+++ config/route.php
@@ -4,9 +4,9 @@ use vendor\System\Router;
 $router = new Router();
 
 $router->setRoute("",  "HomeController/index");
-//$router->setRoute("login",  "LoginController/index");
-$router->setRoute("login",  "AuthenticationController/_Login");
-$router->setRoute("register",  "AuthenticationController/_Register", "register");
+$router->setRoute("login",  "LoginController/index");
+//$router->setRoute("login",  "AuthenticationController/_Login");
+//$router->setRoute("register",  "AuthenticationController/_Register", "register");
 $router->setRoute("data", "DataController/index", 'index', ['GET', 'POST']);
 $router->setRoute("capteur", "CapteurController/index");
 $router->setRoute("docs", "DocumentationController/home", 'HomeDocView');
